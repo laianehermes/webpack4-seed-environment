402a682910aa47ea1cbf6bcc57a47be6daa4469a
laianehermes <laiane.sp@hotmail.com>
2019-07-28 15:23:29 -0300

    modo de dev e produção integrados

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-function generateComponent() {
-    const element__ = document.createElement('DIV');
-    element__.className = 'container';
-    
-    const paragraph = document.createElement('P');
-    const txt = document.createTextNode('Boo! ƪ(ړײ)ƪ');
-    
-    paragraph.appendChild(txt);
-    element__.appendChild(paragraph);
-    
-    console.log('My JavaScript log.');
-  
-    return element__;
+import './base.scss'
+import './assets/stylesheets/typography.scss'
+import MyImg from './assets/images/mountain.jpg'
+class Generator {
+  createParagraph(__string) {
+    const textNode = document.createTextNode(__string);
+    const paragraphElement = document.createElement('P');
+    paragraphElement.appendChild(textNode);
+    return paragraphElement;
   }
-  document.body.appendChild(generateComponent());
-  
+
+  createImg(__source, __width, __height) {
+    const imgElement = new Image(__width, __height);
+    imgElement.src = __source;
+    return imgElement;
+  }
+
+  createDiv(__innerNode) {
+    const divElement = document.createElement('DIV');
+    if (__innerNode) {
+      divElement.appendChild(__innerNode);
+    }
+    return divElement;
+  }
+}
+
+const gen = new Generator;
+const par = gen.createParagraph('Boo! ƪ(ړײ)ƪ');
+const myImg = gen.createImg(MyImg, 250, 400);
+const div = gen.createDiv(par);
+
+div.appendChild(myImg);
+console.log('This is my image address', MyImg);
+
+document.body.appendChild(div);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssets = require('optimize-css-assets-webpack-plugin')
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const devMode = process.env.NODE_ENV === 'development';
 
 module.exports = {
     entry: './src/index.js',
@@ -10,9 +14,51 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin(),
+        new DashboardPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html',
             minify: false
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'assets/images/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true
+    }
+}
+
+if(process.env.NODE_ENV === 'production') {
+    module.exports.plugins.push(
+        new OptimizeCSSAssets()
+    )
 }
